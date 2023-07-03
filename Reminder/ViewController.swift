d64a140e419fe0377f3aadd810db28c88bc02d9a
Vladimir Malyshev <malyshewova@gmail.com>
2023-07-03 14:19:23 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Владимир Малышев on 03.07.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
